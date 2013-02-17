28b4a744448c8c811d80bd5e58b17a9276837588
tin <gimranov.valentin@yandex.ru>
2013-02-17 22:33:09 +0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Like4me::Application.config.secret_token = '2753d93ab32268815895cc130e83480526480d85b283a039d2fd197f91fc8c637697d42c4eb5fbe5528616a634535e72e1e64d16f13bd99634951a59b07a4dbe'
